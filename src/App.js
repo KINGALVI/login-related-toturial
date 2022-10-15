b9c6d0db91b8decae32419c876b3d7f68c7516fc
KINGALVI <alvialmahamud@gmail.com>
2022-10-16 04:41:50 +0600

    added new code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from './Firebase/Firebase';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
 
+  const [user, setUser] = useState({})
   const Provider = new GoogleAuthProvider();
 
   const HandeleGoogleSignIn = () => {
     signInWithPopup(auth, Provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user)
       })
       .catch(error => {
@@ -19,11 +22,36 @@ function App() {
       })
   }
 
+  const HandleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(() => {
+        setUser({});
+      })
+  }
+
   return (
     <div className="App">
-      <button onClick={HandeleGoogleSignIn}>GO TO GOOGLE</button>
+      {
+        user.email ?
+          <button onClick={HandleGoogleSignOut}>
+            GO OUT GOOGLE
+          </button>
+          :
+          <button onClick={HandeleGoogleSignIn}>GO TO GOOGLE</button>
+      }
+
+      {
+        user.email &&
+        <div>
+          <br />
+          <img src={user.photoURL} alt="" />
+          <p>User Name : {user.displayName}</p>
+        </div>
+      }
     </div>
   );
 }
-
 export default App;
